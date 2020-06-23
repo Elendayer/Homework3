73d89bb1da31662e122f92c6a585e4cf8c628ee9
Elendayer <niklas@hoehnk.de>
2020-06-23 19:53:22 +0200

    Renamed Project and Drunkards Walk
    Wall -> Floor Change
    Floor Procent End Condition

---
+++ Homework3_ProceduralContent/HomeWork_ProceduralContent/Assets/Code/DrunkardSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace levelGenerator
+{
+    public class DrunkardSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject _worldGrid;
+        [SerializeField] private GameObject _drunkardManager;
+        [SerializeField] private GameObject _drunkard;
+
+        public void StartDrunkard()
+        {
+            int width;
+            int height;
+            
+            height = _worldGrid.GetComponent<WorldGrid>().Height;
+            width = _worldGrid.GetComponent<WorldGrid>().Width;
+            
+            Instantiate(_drunkard, _drunkardManager.transform);
+        }
+    }
+}
+
+
---
+++ Homework3_ProceduralContent/HomeWork_ProceduralContent/Assets/Code/DrunkardsWalk.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace levelGenerator
+{
+    public class DrunkardsWalk : MonoBehaviour
+    {
+
+        [SerializeField] private float _interval;
+        [SerializeField] private GameObject _floorTile;
+
+        public int _direction;
+
+        private GameObject _worldGrid;
+        private WorldGrid _worldGridScript;
+        
+        private float _floorPercent;
+        [SerializeField] private float _AimedFloorpercent;
+        
+        private int _tileCount;
+        private int currentFloorCount;
+
+        private void Start()
+        {
+            _worldGrid = GameObject.Find("WorldGrid");
+            _worldGridScript = _worldGrid.GetComponent<WorldGrid>();
+            _tileCount = _worldGridScript.Height * _worldGridScript.Width;
+            
+            StartCoroutine("DrunkardsWalkStep");
+        }
+
+        private IEnumerator DrunkardsWalkStep()
+        {
+            while (_AimedFloorpercent >= _floorPercent)
+            {
+                yield return new WaitForSeconds(_interval);
+
+                _direction = Random.Range(1, 5);
+
+                Vector2 currentPosition;
+                currentPosition = this.transform.position;
+
+                if (_direction == 1)
+                {
+                    this.gameObject.transform.position = new Vector2(currentPosition.x - 1, currentPosition.y);
+                }
+
+                if (_direction == 2)
+                {
+                    this.gameObject.transform.position = new Vector2(currentPosition.x + 1, currentPosition.y);
+                }
+
+                if (_direction == 3)
+                {
+                    this.gameObject.transform.position = new Vector2(currentPosition.x, currentPosition.y - 1);
+                }
+
+                if (_direction == 4)
+                {
+                    this.gameObject.transform.position = new Vector2(currentPosition.x, currentPosition.y + 1);
+                }
+            }
+        }
+        private void Update()
+        {
+            currentFloorCount = _worldGridScript.FloorLayer.transform.childCount;
+            
+            _floorPercent = ((float) currentFloorCount/ (float) _tileCount) * 100 ;
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.transform.parent == _worldGrid.GetComponent<WorldGrid>().WallLayer.transform)
+            {
+                Instantiate(_floorTile, _worldGrid.GetComponent<WorldGrid>().FloorLayer.transform);
+                _floorTile.transform.position = other.transform.position;
+       
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
+
